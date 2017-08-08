c01b97b16d2a3e39a77c157976a17d357515677b
Levi Jarick <github@ubeek.net>
2017-08-08 13:54:21 +0800

    Update readme; Add failure for manage_proxy = true && proxy_address = undef

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,13 @@
 # == Class vault::service
 class vault::service {
   if $::vault::manage_service {
+    
+    if $::vault::manage_proxy {
+      if $::vault::proxy_address == undef {
+        fail("manage_proxy is true, but proxy address has not been set!")
+      }
+    }
+    
     service { $::vault::service_name:
       ensure   => running,
       enable   => true,
