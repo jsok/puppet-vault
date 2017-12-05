d93b07a403c276b7ad9f3891e6b531f5fd59980d
Jonathan Sokolowski <jsok@users.noreply.github.com>
2017-12-05 21:04:40 +1100

    Use alternative module_install_helper helpers

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -3,8 +3,8 @@ require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 
 run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
-install_module_on(hosts)
-install_module_dependencies_on(hosts)
+install_module
+install_module_dependencies
 
 RSpec.configure do |c|
   # Readable test descriptions
