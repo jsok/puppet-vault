0ecd92f04ccc977e45e6f9b95f04ad2f4b69ef4b
Tim Meusel <tim@bastelfreak.de>
2018-05-31 01:07:09 +0200

    enhance acceptance tests

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -107,6 +107,9 @@ describe 'vault class' do
     describe port(8200) do
       it { is_expected.to be_listening.on('127.0.0.1').with('tcp') }
     end
+    describe command('vault version') do
+      its(:stdout) { is_expected.to match %r{v0\.10\.0} }
+    end
   end
 
   # we will test if we can update vault to another version
@@ -131,6 +134,17 @@ describe 'vault class' do
       # Run it twice and test for idempotency
       apply_manifest(pp, catch_failures: true)
       apply_manifest(pp, catch_changes: true)
+    end
+    describe service('vault') do
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
+    end
+
+    describe port(8200) do
+      it { is_expected.to be_listening.on('127.0.0.1').with('tcp') }
+    end
+    describe command('vault version') do
+      its(:stdout) { is_expected.to match %r{v0\.10\.1} }
     end
   end
 end
