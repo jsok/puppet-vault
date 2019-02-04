97f0d75fa466de722ab8cdce24cd2557cc652edc
Luke McClelland <lmcclell@users.noreply.github.com>
2019-02-04 21:15:33 +1100

    Remove leading 'v' from verison fact
    Other facts (including puppet and facter itself) which have version do not have leading 'v'.

--- lib/facter/vault_version.rb
+++ lib/facter/vault_version.rb
@@ -3,10 +3,10 @@ require 'facter'
 
 vault_bin = Facter::Util::Resolution.exec("which vault")
 if File.exists?("#{vault_bin}")
-  version = Facter::Util::Resolution.exec("#{vault_bin} --version | egrep -o 'v[0-9].* '")
+  version = Facter::Util::Resolution.exec("#{vault_bin} --version | grep -oE 'v[0-9.]* '")
   Facter.add("vault_version") do
     setcode do
-      "#{version}"
+      "#{version}".sub(/^v/, "")
     end
   end
 end
