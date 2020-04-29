9dfb00393c224c3bf7f0364cea4411147dadda3a
John Schoewe <john.schoewe@encore.tech>
2020-04-29 12:46:38 -0400

    Converted rest-client functions to use net/https

--- lib/puppet/provider/vault_cert/openssl.rb
+++ lib/puppet/provider/vault_cert/openssl.rb
@@ -1,6 +1,8 @@
 require 'pathname'
 require 'time'
-require 'rest-client'
+require 'net/https'
+#require 'net/ssh'
+#require 'net/scp'
 require 'json'
 # prepreqs
 # - assume a certificate on the filesystem
@@ -41,14 +43,78 @@ Puppet::Type.type(:vault_cert).provide(:openssl) do
   #  cert.check_private_key(priv)
   #end
 
+  # Return a Net::HTTP::Response object
+  def send_request(operation = 'GET', path = '', data = nil, headers = {}, search_path = {})
+    request = nil
+    encoded_search = ''
+
+    vault_scheme = 'http'
+    vault_server = resource[:vault_server]
+    vault_port = resource[:vault_port]
+
+
+    if URI.respond_to?(:encode_www_form)
+      encoded_search = URI.encode_www_form(search_path)
+    else
+      # Ideally we would have use URI.encode_www_form but it isn't
+      # available with Ruby 1.8.x that ships with CentOS 6.5.
+      encoded_search = search_path.to_a.map do |x|
+        x.map { |y| CGI.escape(y.to_s) }.join('=')
+      end
+      encoded_search = encoded_search.join('&')
+    end
+    uri = URI.parse format('%s://%s:%d%s?%s', vault_scheme, vault_server, vault_port, path, encoded_search)
+
+    case operation.upcase
+    when 'POST'
+      request = Net::HTTP::Post.new(uri.request_uri, headers)
+      request.body = data.to_json
+    when 'PUT'
+      request = Net::HTTP::Put.new(uri.request_uri, headers)
+      request.body = data.to_json
+    when 'GET'
+      request = Net::HTTP::Get.new(uri.request_uri, headers)
+    when 'DELETE'
+      request = Net::HTTP::Delete.new(uri.request_uri, headers)
+    else
+      raise Puppet::Error, format('Unsupported HTTP operation %s', operation)
+    end
+
+    request.content_type = 'application/json'
+    #if resource[:grafana_user] && resource[:grafana_password]
+    #  request.basic_auth resource[:grafana_user], resource[:grafana_password]
+    #end
+    resp = nil
+    Net::HTTP.start(vault_server, vault_port,
+                    use_ssl: vault_scheme == 'https',
+                    verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
+      resp = http.request(request)
+    end
+    
+    # check response for success, redirect or error
+    case resp
+    when Net::HTTPSuccess then
+      resp
+    when Net::HTTPRedirection then
+      send_request(operation, resp['location'], data, headers)
+    else
+      message = 'code=' + resp.code
+      message += ' message=' + resp.message
+      message += ' body=' + resp.body
+      raise resp.error_type.new(message, resp)
+    end
+  end  
+    
   def get_client_fqdn
+    Puppet.info(Facter.value('fqdn'))
     return @client_fqdn unless @client_fqdn.nil?
     @client_fqdn = Facter.value('fqdn')
   end
 
-  def generate_cert
+  def create_cert
+    Puppet.info("create_cert")
     common_name = get_client_fqdn
-    url = 'http://' + resource[:vault_server] + ':' + resource[:vault_port] + '/v1/' + resource[:secret_engine] + '/issue/' + resource[:secret_role]
+    api_path = '/v1/' + resource[:secret_engine] + '/issue/' + resource[:secret_role]
 
     headers = {
       "X-Vault-Token" => resource[:api_token]
@@ -60,14 +126,22 @@ Puppet::Type.type(:vault_cert).provide(:openssl) do
       ttl: resource[:ttl_hours]
     }
 
-    response = RestClient.post(url, payload.to_json, headers)
-
-    response_body = JSON.parse(response.body)
-    return response_body
+    response = send_request('POST', api_path, payload, headers)
+    if response.body
+      return JSON.parse(response.body)
+    else
+      return response
+    end
   end
 
-  def revoke_cert(serial_number)
-    url = 'http://' + resource[:vault_server] + ':8200/v1/' + resource[:secret_engine] + '/revoke'
+  def revoke_cert
+    cert = get_certificate
+    # Get the serial number from the cert object
+    serial_number = cert.serial.to_s(16)
+    # Add colons to the returned serial number
+    serial_number = serial_number.scan(/\w{2}/).join(':')
+
+    api_path = '/v1/' + resource[:secret_engine] + '/revoke'
 
     headers = {
       "X-Vault-Token" => resource[:api_token]
@@ -77,9 +151,7 @@ Puppet::Type.type(:vault_cert).provide(:openssl) do
       serial_number: serial_number
     }
 
-    response = RestClient.post(url, payload.to_json, headers)
-    puts response
-
+    response = send_request('POST', api_path, payload, headers)
     if response.body
       return JSON.parse(response.body)
     else
@@ -139,21 +211,30 @@ Puppet::Type.type(:vault_cert).provide(:openssl) do
                 end
   end
 
-  def save_certificate
+  def client_cert_save(cert)
     common_name = get_client_fqdn
-    cert = get_certificate
-    priv_key = get_private_key
-    # Save the new cert in the certs directory on the vault server
-    cert_path = '/opt/vault/certs/' + common_name + '.crt'
+    # Save the new cert in the certs directory on the client server
+    cert_path = '/etc/pki/tls/certs/' + common_name + '.crt'
     File.open(cert_path, 'w') do |f|
-        f.write(cert)
+      f.write(cert['data']['certificate'])
     end
 
     # Save the new private key in the tls directory on the client
-    key_path = '/etc/pki/tls/certs/' + common_name + '.pem'
+    key_path = '/etc/pki/tls/private/' + common_name + '.pem'
     File.open(key_path, 'w') do |f|
-      f.write(priv_key)
+      f.write(cert['data']['private_key'])
     end
+  end
+
+  def vault_cert_save(cert)
+    common_name = get_client_fqdn
+    # Save the new cert in the certs directory on the vault server
+    cert_path = '/opt/vault/certs/' + common_name + '.crt'
+    File.open(cert_path, 'w') do |f|
+      f.write(cert['data']['certificate'])
+    end
+
+
   end
 
   def exists?
@@ -164,19 +245,25 @@ Puppet::Type.type(:vault_cert).provide(:openssl) do
     #  - check if cert is revoked (if we want to get crazy)
     #    - if we have the cert ID, we can ask Vault for this
     cert = get_certificate
+    Puppet.info(cert.serial)
     priv_key = get_private_key
     if cert && priv_key
       if !cert.check_private_key(priv_key)
+        Puppet.info("FALSE")
         return false
       end
       if cert_expiring
-        return false
+        Puppet.info("TRUE")
+        return true
+        #return false
       end
       #unless self.class.old_cert_is_equal(resource)
       #  return false
       #end
+      Puppet.info("TRUE 2")
       true
     else
+      Puppet.info("FALSE 2")
       false
     end
   end
@@ -188,7 +275,10 @@ Puppet::Type.type(:vault_cert).provide(:openssl) do
     # This is where we want to use the Grafana resource reference
     Puppet.info("CREATE")
     #cert = get_certificate
-    save_certificate
+    #save_certificate
+    new_cert = create_cert
+    client_cert_save(new_cert)
+    vault_cert_save(new_cert)
   end
 
   def destroy
@@ -196,6 +286,7 @@ Puppet::Type.type(:vault_cert).provide(:openssl) do
     #  - delete the cert off the filesystem
     #  - revoke the cert in Vault
     Puppet.info("DESTROY")
+    revoke_cert
     #Pathname.new(resource[:cert_path]).delete
   end
 
