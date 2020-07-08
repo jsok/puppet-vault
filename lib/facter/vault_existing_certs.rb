fb14020dfb97311087fe30538d3b95add0feeb50
Nick Maludy <nmaludy@gmail.com>
2020-07-08 13:12:49 -0400

    Fix problems with cert name fact

--- lib/facter/vault_existing_certs.rb
+++ lib/facter/vault_existing_certs.rb
@@ -62,8 +62,8 @@ Facter.add(:vault_existing_certs) do
       foreach ($cert in $cert_list) {
         $cert_data = @{}
         $path = $cert.PSPath.Replace('Microsoft.PowerShe.Security\\Certificate::', 'Cert:\\')
-        $cert_data['common_name'] = $cert.SubjectName.name
-        $cert_data['cert_name'] = $cert.FriendlyName.
+        $cert_data['common_name'] = $cert.SubjectName.Name
+        $cert_data['cert_name'] = $cert.FriendlyName
         $cert_data['not_after'] = $cert.NotAfter.ToString("o")  # Convert to ISO format
         $cert_data['not_before'] = $cert.NotBefore.ToString("o")
         $cert_data['path'] = $path
