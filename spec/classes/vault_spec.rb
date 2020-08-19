4fe1a19fabbbf7ed1dea9f04e282c5ddce214ade
Rudy Gevaert <66934+rgevaert@users.noreply.github.com>
2020-08-19 16:17:04 +0200

    Add support for raft storage

--- spec/classes/vault_spec.rb
+++ spec/classes/vault_spec.rb
@@ -297,12 +297,32 @@ describe 'vault' do
         }
       end
 
-      context 'when specifying manage_storage_dir' do
+      context 'when specifying manage_storage_dir and file storage backend' do
         let(:params) do
           {
             manage_storage_dir: true,
             storage: {
               'file' => {
+                'path' => '/data/vault'
+              }
+            }
+          }
+        end
+
+        it {
+          is_expected.to contain_file('/data/vault').
+            with_ensure('directory').
+            with_owner('vault').
+            with_group('vault')
+        }
+      end
+
+      context 'when specifying manage_storage_dir and raft storage backend' do
+        let(:params) do
+          {
+            manage_storage_dir: true,
+            storage: {
+              'raft' => {
                 'path' => '/data/vault'
               }
             }
