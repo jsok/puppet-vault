e036ff0249851d860f46ab32ebec0b6c7b34b584
Rudy Gevaert <66934+rgevaert@users.noreply.github.com>
2020-09-07 13:52:14 +0200

    Restart vault daemon on config file change
    See https://github.com/jsok/puppet-vault/pull/154

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,34 +1,60 @@
 # == Class vault::install
 #
 class vault::install {
+
   $vault_bin = "${::vault::bin_dir}/vault"
 
   case $::vault::install_method {
-      'archive': {
-        if $::vault::manage_download_dir {
-          file { $::vault::download_dir:
-            ensure => directory,
-          }
+    'archive': {
+      if $::vault::manage_download_dir {
+        file { $::vault::download_dir:
+          ensure => directory,
         }
-
-        archive { "${::vault::download_dir}/${::vault::download_filename}":
-          ensure       => present,
-          extract      => true,
-          extract_path => $::vault::bin_dir,
-          source       => $::vault::real_download_url,
-          cleanup      => true,
-          creates      => $vault_bin,
-          before       => File['vault_binary'],
-        }
-
-        $_manage_file_capabilities = true
       }
+
+      $_manage_file_capabilities = true
+      $_vault_versioned_bin = "/opt/vault-${::vault::version}/vault"
+
+      file { "/opt/vault-${::vault::version}":
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+      }
+
+      archive { "${::vault::download_dir}/${::vault::download_filename}":
+        ensure       => present,
+        extract      => true,
+        extract_path => "/opt/vault-${::vault::version}",
+        source       => $::vault::real_download_url,
+        cleanup      => true,
+        creates      => $_vault_versioned_bin,
+        before       => File['vault_binary'],
+      }
+
+      file { 'vault_binary':
+        path   => $vault_bin,
+        target => $_vault_versioned_bin,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+        notify => Service[$::vault::service_name],
+      }
+
+    }
 
     'repo': {
       package { $::vault::package_name:
         ensure  => $::vault::package_ensure,
       }
       $_manage_file_capabilities = false
+
+      file { 'vault_binary':
+        path  => $vault_bin,
+        owner => 'root',
+        group => 'root',
+        mode  => '0755',
+      }
     }
 
     default: {
@@ -36,17 +62,10 @@ class vault::install {
     }
   }
 
-  file { 'vault_binary':
-    path  =>  $vault_bin,
-    owner => 'root',
-    group => 'root',
-    mode  => '0755',
-  }
-
   if !$::vault::disable_mlock and pick($::vault::manage_file_capabilities, $_manage_file_capabilities) {
     file_capability { 'vault_binary_capability':
       ensure     => present,
-      file       => $vault_bin,
+      file       => pick($_vault_versioned_bin, $vault_bin),
       capability => 'cap_ipc_lock=ep',
       subscribe  => File['vault_binary'],
     }
