eaa1c94dc9446e96e4798841bf9cc4bbc044c629
Bram Vogelaar <bram@attachmentgenie.com>
2021-01-17 22:07:12 +0100

    Configure repo before package (#178)

--- manifests/install.pp
+++ manifests/install.pp
@@ -27,6 +27,7 @@ class vault::install {
     'repo': {
       if $vault::manage_repo{
         include hashi_stack::repo
+        Class['hashi_stack::repo'] -> Package[$::vault::package_name]
       }
       package { $::vault::package_name:
         ensure  => $::vault::package_ensure,
